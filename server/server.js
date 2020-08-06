6a57abe6054e61ce707964f5ede0aa3c0e39ded8
phitranpt <phi.tran.pt@gmail.com>
2020-08-05 23:42:30 -0500

    fixed server.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 5000;
 
-app.use(express.static(publicPath));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
-});
+app.use(express.static('build'));
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}!`);
